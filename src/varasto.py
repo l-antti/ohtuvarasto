d31f73208bbfc660bc1d1221c5abf73d4b9502bc
l-antti <laura.k.anttila@gmail.com>
2024-11-10 20:00:01 +0200

    fix typo

--- src/varasto.py
+++ src/varasto.py
@@ -43,6 +43,6 @@ class Varasto:
         return maara
 
     def __str__(self):
-        saldo_str = f"saldo = {self.saldo} "
+        saldo_str = f"saldo = {self.saldo}, "
         tilavuus_str = f"viel tilaa {self.paljonko_mahtuu()}"
-        return saldo_str + ", " + tilavuus_str
+        return saldo_str + tilavuus_str
